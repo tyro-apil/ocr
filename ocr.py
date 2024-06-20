627a92476ae576ea57aa5a236dcc6ac4e97eea17
apil <078bct017.apil@pcampus.edu.np>
2024-06-20 13:14:33 +0545

    feat: fix numpy import problem by using PIL images

--- ocr.py
+++ ocr.py
@@ -1,54 +1,55 @@
-from paddleocr import PaddleOCR,draw_ocr
-import cv2
+from paddleocr import PaddleOCR, draw_ocr
 from PIL import Image
 
-def extract_text(img, ocr):
-  """
-  Returns list detected text with bbox, text and confidence value in following format:
-  [bbox, (text,confidence), ....]
-  """
-  results = ocr.ocr(img, cls=True)
-  result = results[0]
-  return result
+
+def extract_text(img_path, ocr):
+    """
+    Returns list detected text with bbox, text and confidence value in following format:
+    [bbox, (text,confidence), ....]
+    """
+    results = ocr.ocr(img_path, cls=True)
+    result = results[0]
+    return result
+
 
 def parse_result(result):
-  """
-  Returns a tuple of bboxes_list, detected_text_list, confidence_list
-  """
-  boxes = [line[0] for line in result]
-  txts = [line[1][0] for line in result]
-  scores = [line[1][1] for line in result]
-  return boxes, txts, scores
+    """
+    Returns a tuple of bboxes_list, detected_text_list, confidence_list
+    """
+    boxes = [line[0] for line in result]
+    txts = [line[1][0] for line in result]
+    scores = [line[1][1] for line in result]
+    return boxes, txts, scores
 
-def get_annotated_img(img, boxes, txts, scores):
-  """
-  Draws bounding boxes on the image and detected texts
-  """
-  # if font path unset, problem in google-colab to execute
-  # dont know whats the problem
-  image = draw_ocr(img, boxes, txts, scores, font_path='/usr/share/fonts/truetype/humor-sans/Humor-Sans.ttf')
-  pil_img = Image.fromarray(image)
-  return pil_img
 
-ocr = PaddleOCR(use_angle_cls=True, lang='en') # need to run only once to download and load model into memory
+def get_annotated_img(img_path, boxes, txts, scores):
+    """
+    Returns PIL image by drawing bounding boxes on the image and detected texts
+    """
+    img = Image.open(img_path).convert("RGB")
+    im_show = draw_ocr(img, boxes, txts, scores, font_path="simfang.ttf")
+    im_show = Image.fromarray(im_show)
+    return im_show
+
+# need to run only once to download and load model into memory
+ocr = PaddleOCR(
+    use_angle_cls=True, lang="en"
+)  
 
 # Load image
-img_path = 'test.jpeg'
-img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
+img_path = "test.jpeg"
 
 # Extract text
-result = extract_text(img, ocr)
+result = extract_text(img_path, ocr)
 
 # Parse result
 boxes, txts, scores = parse_result(result)
 
 # Get annotated image
-annotated_img = get_annotated_img(img, boxes, txts, scores)
+annotated_img = get_annotated_img(img_path, boxes, txts, scores)
 
 # Display image
-cv2.imshow('Annotated Image', annotated_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+annotated_img.show()
 
 # Save image
-cv2.imwrite('annotated_img.jpg', annotated_img)
+annotated_img.save("annotated_img.jpg")
